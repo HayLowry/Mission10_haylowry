3f7373346e5834a54eadc580840ed4ec4ab01e73
haylowry <haylow77@gmail.com>
2023-03-08 09:53:19 -0700

    Add project files.

---
+++ Mission10_haylowry/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission10_haylowry.Models;
+using Mission10_haylowry.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission10_haylowry.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository Repo { get; set; }
+        public HomeController (IBookstoreRepository temp)
+        {
+            Repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var models = new BooksViewModel
+            {
+                Books = Repo.Books
+                .OrderBy(x => x.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = Repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+
+            };
+ 
+
+            return View(models);
+        }
+
+    }
+}
---
+++ Mission10_haylowry/Models/IBookstoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission10_haylowry.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+
+    }
+}
